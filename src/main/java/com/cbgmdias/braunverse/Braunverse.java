4c817797cc110b86d4ae3b29c51c0fd0f1e5e9e5
Cristhian Braun <114692497+CBGMDias@users.noreply.github.com>
2025-04-10 22:40:30 -0300

    Configurando registro de itens, texturas, modelos, lang, add na aba criativo. Adicionado: Braunite Azuronite Raw Azuronite

--- src/main/java/com/cbgmdias/braunverse/Braunverse.java
+++ src/main/java/com/cbgmdias/braunverse/Braunverse.java
@@ -1,6 +1,9 @@
 package com.cbgmdias.braunverse;
 
+import com.cbgmdias.braunverse.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -33,6 +36,8 @@ public class Braunverse {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -46,7 +51,11 @@ public class Braunverse {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BRAUNITE);
+            event.accept(ModItems.RAW_AZURONITE);
+            event.accept(ModItems.AZURONITE_INGOT);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/cbgmdias/braunverse/item/ModItems.java
@@ -0,0 +1,24 @@
+package com.cbgmdias.braunverse.item;
+
+import com.cbgmdias.braunverse.Braunverse;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Braunverse.MOD_ID);
+
+    public static final RegistryObject<Item> BRAUNITE = ITEMS.register("braunite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_AZURONITE = ITEMS.register("raw_azuronite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> AZURONITE_INGOT = ITEMS.register("azuronite_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
